22b696acaedcabb6f1d4862fd6e036b749a54ec2
Ammar-muz <ammarmuzzammil43@gmail.com>
2026-08-16 17:25:30 +0530

    db update

--- frontend/lib/db.js
+++ frontend/lib/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import "./models/User";
+import "./models/Item";
+import "./models/Booking";
+import "./models/Message";
 
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) {
